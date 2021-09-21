345905a45f1b804ba3d5cd56530c80821682c90c
yyg900xp <79534137+yyg900xp@users.noreply.github.com>
2021-09-22 04:49:40 +0800

    Update stoppingtime.m

--- chapter4/section 4.2.2.2/stoppingtime.m
+++ chapter4/section 4.2.2.2/stoppingtime.m
@@ -1,3 +1,7 @@
+%this file is for computing the stopping time T_t =inf{s>0, tau_s -a >t } where tau_s is the alpha-stable subordinator 
+%the stblrnd is a function which can generate data that are stable distributed S(alpha,beta,scale,delta).
+%Y is the path of the alpha-stable subordinator.
+
 function stoptime = stoppingtime(alpha,beta,scale,delta,desired_time,n,a)
 X = stblrnd(alpha,beta,scale,delta,n,1);
 Y = cumsum(X);
