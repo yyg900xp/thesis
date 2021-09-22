2fc37f5b02a755fa9e52aca55b7863b63b68ca73
yyg900xp <79534137+yyg900xp@users.noreply.github.com>
2021-09-22 17:51:20 +0800

    Update int2.m

--- chapter4/section 4.2.2.2/int2.m
+++ chapter4/section 4.2.2.2/int2.m
@@ -1,3 +1,6 @@
+%according to the formula of the Monte Carlo estimator, the variance of the increments of the Brownian motion is changing 
+%we define a function int2 to represent t_i^k=(i-1)*k where difference here refers to k.
+
 function intger2 = int2 (x,difference,c)
 intger2 = zeros(c,1);
 for i=1:x
